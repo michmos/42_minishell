aebaac64669f9773836c678186d9ef4adb91dfc2
Michael Moser <Michael.Moser11@web.de>
2024-04-24 12:19:17 +0200

    Move functions to libft
    These functions are commonly used in other projects
    as well, so it makes more sense to integrate them
    into libft

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -1,44 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmoser <mmoser@student.codam.nl>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/24 12:17:46 by mmoser            #+#    #+#             */
+/*   Updated: 2024/04/24 12:17:49 by mmoser           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "utils.h"
 
-int	ft_min(int num1, int num2)
-{
-	if (num1 < num2)
-	{
-		return (num1);
-	}
-	else
-	{
-		return (num2);
-	}
-}
-
-int	ft_max(int num1, int num2)
-{
-	if (num1 > num2)
-	{
-		return (num1);
-	}
-	else
-	{
-		return (num2);
-	}
-}
-
-void	*ft_realloc(void *ptr, size_t new_size, size_t old_size)
-{
-	void	*new;
-
-	new = malloc(new_size);
-	if (!new)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	if (ptr)
-	{
-		ft_memcpy(new, ptr, ft_min(new_size, old_size));
-	}
-	free(ptr);
-	return (new);
-}
--- src/utils/utils.h
+++ src/utils/utils.h
@@ -4,10 +4,4 @@
 
 # include "../../external_libs/42_libs/ft_libs.h"
 
-int		ft_min(int num1, int num2);
-int		ft_max(int num1, int num2);
-void	*ft_realloc(void *ptr, size_t new_size, size_t old_size);
-
-
 #endif // !UTILS_H
-# define UTILS_H
