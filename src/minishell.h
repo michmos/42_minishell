8529ad507f1158fd1b96940b39a41534a0c5036e
Michael Moser <Michael.Moser11@web.de>
2024-04-25 13:06:39 +0200

    Add structs

--- src/minishell.h
+++ src/minishell.h
@@ -5,4 +5,32 @@
 # include "../external_libs/42_libs/ft_libs.h"
 # include "../src/utils/utils.h"
 
-#endif // !MINISHELL_H
+typedef struct	s_token
+{
+	t_tag			tag;
+	char			*lexeme;
+	struct s_token	*next;
+} t_token;
+
+typedef struct s_parse_str
+{
+	char	*buf;
+	size_t	buf_len;
+	size_t	cursor_pos;
+} t_parse_str;
+
+typedef struct s_vec
+{
+	char	*buf;
+	size_t	size;
+} t_vec;
+
+typedef struct s_AST
+{
+	t_tag			tag;
+	char			*buf;
+	struct s_AST	**next;
+
+} t_AST;
+
+#endif
