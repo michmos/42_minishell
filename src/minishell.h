95759156471d1c138ebe128e7c17349c6e4081a4
Paulius Minialga <pminialg@f1r1s17.codam.nl>
2024-05-21 11:39:32 +0200

    Update header file

--- src/minishell.h
+++ src/minishell.h
@@ -16,7 +16,7 @@ typedef enum e_tag
 	SI_QUO,
 	DU_QUO,
 	WORD
-} t_tag;
+}	t_tag;
 
 typedef enum e_error
 {
@@ -25,26 +25,26 @@ typedef enum e_error
 
 	// Syscall fails
 	SYS_ERR
-} t_error;
+}	t_error;
 
-typedef struct	s_token
+typedef struct s_token
 {
 	t_tag			tag;
 	char			*lexeme;
-} t_token;
+}	t_token;
 
 typedef struct s_parse_str
 {
 	char	*buf;
 	size_t	buf_len;
 	size_t	cursor_pos;
-} t_parse_str;
+}	t_parse_str;
 
 typedef struct s_vec
 {
 	char	*buf;
 	size_t	size;
-} t_vec;
+}	t_vec;
 
 typedef struct s_AST
 {
@@ -52,23 +52,55 @@ typedef struct s_AST
 	char			*buf;
 	struct s_AST	**next;
 
-} t_AST;
+}	t_AST;
 
 // --------------------------- PARSING/ --------------------------------------//
 // lex_add_ops.c -------------------------------------------------------------//
-t_error	add_word(t_parse_str *cmd_line, t_vec *lexeme);
-t_error	add_str_literal(t_parse_str *cmd_line, t_vec *lexeme);
-t_error	add_char(char c, t_vec *vector);
+t_error		add_word(t_parse_str *cmd_line, t_vec *lexeme);
+t_error		add_str_literal(t_parse_str *cmd_line, t_vec *lexeme);
+t_error		add_char(char c, t_vec *vector);
 
 // lex_char_ops.c ------------------------------------------------------------//
-char	peek_char(t_parse_str *cmd_line);
-char	next_char(t_parse_str *cmd_line);
-char	cur_char(t_parse_str *str);
+char		peek_char(t_parse_str *cmd_line);
+char		next_char(t_parse_str *cmd_line);
+char		cur_char(t_parse_str *str);
 
 // lexer.c -------------------------------------------------------------------//
-t_list	*lexer(t_parse_str *cmd_line);
+t_list		*lexer(t_parse_str *cmd_line);
 
 // ---------------------------- UTILS/ ---------------------------------------//
 // utils.c -------------------------------------------------------------------//
 
+// ---------------------------- BUILTINS/ ------------------------------------//
+typedef struct s_env_var
+{
+	char			*key; //before equal sign
+	char			*value; //after equal sign
+	bool			equal;
+}	t_env_var;
+
+typedef struct s_parse_env
+{
+	char	*buffer;
+	size_t	buffer_len;
+	size_t	cursor_pos;
+}	t_parse_env;
+
+// builtins.c ---------------------------------------------------------------//
+void		print_envlst(t_list *head);
+
+// env.c --------------------------------------------------------------------//
+char		*get_key(t_env_var *env_var, t_parse_env *env_parse);
+char		*get_value(t_env_var *env_var, t_parse_env *env_parse);
+t_env_var	*get_env_var(char *env);
+t_list		*create_envlst(char **env);
+void		free_env_var(void *node);
+
+// ordered_env.c -----------------------------------------------------------//
+t_list		*create_ordered_envlst(t_list *env);
+char		*key(t_list *node);
+
+// add_to_list.c -----------------------------------------------------------//
+t_list		*add_to_ordered_envlst(t_list *head, char *argv);
+t_list		*add_to_envlst(t_list *head, char *argv);
 #endif
