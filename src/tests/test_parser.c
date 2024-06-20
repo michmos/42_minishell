67c584dd738df17f38b63edd8c2295e91b5cf1e3
Michael Moser <Michael.Moser11@web.de>
2024-06-20 11:08:57 +0200

    Add parser tester

---
+++ src/tests/test_parser.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_parser.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmoser <mmoser@student.codam.nl>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/02 11:51:51 by mmoser            #+#    #+#             */
+/*   Updated: 2024/06/19 11:35:25 by mmoser           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+#include <stdio.h>
+
+void	print_token_lst(t_list *head)
+{
+	char *enum_strs[] = {
+		"WHITESPACES",
+		"I_RD",
+		"I_RD_HD",
+		"O_RD",
+		"O_RD_APP",
+		"PIPE",
+		"SI_QUO",
+		"DU_QUO",
+		"WORD",
+		NULL
+	};
+
+	while (head)
+	{
+		printf("[%s:\"%s\"]", enum_strs[((t_token *)(head->as_ptr))->tag], ((t_token *)(head->as_ptr))->lexeme);
+		head = head->next;
+		if (head)
+			printf("  ->  ");
+	}
+}
+
+void	print_cmds(t_list *cmds)
+{
+	size_t	i;
+	size_t	j;
+	t_cmd	*cmd;
+
+	i = 0;
+	while(cmds)
+	{
+		cmd = (t_cmd *)(cmds->as_ptr);
+		printf("cmd #%zu\n", i);
+
+		// print args array
+		printf("- args arrary: ");
+		j = 0;
+		while (cmd->args[j])
+		{
+			printf("\"%s\" ", cmd->args[j]);
+			j++;
+		}
+		printf("\n");
+
+		// print redirs linked list
+		printf("- redirections list: ");
+		print_token_lst(cmd->redir_lst);
+
+		cmds = cmds->next;
+		if (cmds)
+			printf("\n\n |\n v\n\n");
+		i++;
+	}
+}
+
+int main(int argc, char *argv[], char **env)
+{
+	t_list		*cmds;
+	t_list		*env_lst;
+	t_error	error;
+
+	if (argc != 2)
+	{
+		printf("Usage '<ARGS>'");
+		return (1);
+	}
+	env_lst = create_envlst(env);
+	cmds = NULL;
+	error = parsing(&cmds, argv[1], env_lst);
+	print_cmds(cmds);
+	return (0);
+}
