7c33e81886fab78ec5675a6159bc93f19382dfe9
Michael Moser <Michael.Moser11@web.de>
2024-09-13 17:32:09 +0200

    Fix env expansion with multi word values
    Imagine: export WORDS="one two"
    previously $WORDS expanded to "one two"
    instead of "one" "two"

--- src/parsing/parser/extend_arg_lst.c
+++ src/parsing/parser/extend_arg_lst.c
@@ -12,14 +12,14 @@
 
 #include "../../minishell.h"
 
-char	*get_arg(t_list **rem_tokens)
+char	*get_expanded_str(t_list **rem_tokens)
 {
 	t_token	token;
-	char	*arg;
+	char	*str;
 	char	*temp;
 
 	temp = NULL;
-	arg = NULL;
+	str = NULL;
 	while (*rem_tokens && is_literal(get_token_tag(*rem_tokens)))
 	{
 		token = pop_token(rem_tokens);
@@ -28,40 +28,91 @@ char	*get_arg(t_list **rem_tokens)
 			if (expand_all_env_vars(&token.lexeme) != NO_ERR)
 			{
 				free(token.lexeme);
-				free(arg);
+				free(str);
 				return (NULL);
 			}
 		}
-		temp = ft_strjoin(arg, token.lexeme);
+		temp = ft_strjoin(str, token.lexeme);
 		free(token.lexeme);
-		free(arg);
+		free(str);
 		if (!temp)
 		{
 			perror("malloc");
 			return (NULL);
 		}
-		arg = temp;
+		str = temp;
 	}
-	return (arg);
+	return (str);
 }
+
+static char	**get_args(t_list **rem_tokens)
+{
+	char	**arr;
+	char	*str;
+	t_tag	tag;
+
+	tag = get_token_tag(*rem_tokens);
+	str = get_expanded_str(rem_tokens);
+	if (!str)
+	{
+		return (NULL);
+	}
+
+	if (tag == WORD)
+	{
+		arr = ft_split(str, ' ');
+	}
+	else
+	{
+		arr = ft_calloc(2, sizeof(char *));
+		arr[0] = str;
+	}
+	if (!arr)
+	{
+		perror("malloc");
+		free(str);
+		return (NULL);
+	}
+	return (arr);
+}
+
+static t_error	add_to_arg_lst(t_list **args_lst, char **args)
+{
+	t_list	*new;
+	size_t	i;
+
+	i = 0;
+	while (args[i])
+	{
+		new = ft_lstnew(args[i]);
+		if (!new)
+		{
+			perror("malloc");
+			return (DEADLY_ERR);
+		}
+		ft_lstadd_back(args_lst, new);
+		i++;
+	}
+	return (NO_ERR);
+}
+
 
 t_error	extend_arg_lst(t_list **args_lst, t_list **rem_tokens)
 {
-	t_list	*new;
-	char	*arg;
+	char	*str;
+	char	**arr;
+	t_tag	tag;
 
-	arg = get_arg(rem_tokens);
-	if (!arg)
+	arr = get_args(rem_tokens);
+	if (!arr)
 	{
 		return (DEADLY_ERR);
 	}
-	new = ft_lstnew(arg);
-	if (!new)
+	
+	if (add_to_arg_lst(args_lst, arr) != NO_ERR)
 	{
-		free(arg);
-		perror("malloc");
+		ft_free_2d_array((void **) arr);
 		return (DEADLY_ERR);
 	}
-	ft_lstadd_back(args_lst, new);
 	return (NO_ERR);
 }
