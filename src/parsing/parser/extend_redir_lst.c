aee99d62c91642fb74d462b62edc07c0d9622b54
Michael Moser <Michael.Moser11@web.de>
2024-09-13 13:34:01 +0200

    Fix multi token filenames
    Previously we would only look at the very next token
    after the redir symbol, to find the filename.
    The filename can consist of multiple tokens though
    like: >> file"name"'1'

--- src/parsing/parser/extend_redir_lst.c
+++ src/parsing/parser/extend_redir_lst.c
@@ -12,31 +12,67 @@
 
 #include "../../minishell.h"
 
-static t_error	parse_redir(t_redir **redir, t_list **rem_tokens)
+static char	*get_redir_filename(t_list **rem_tokens)
 {
 	t_token	token;
+	char	*filename;
+	char	*temp;
+
+	filename = NULL;
+	while (*rem_tokens && is_literal(get_token_tag(*rem_tokens)))
+	{
+		token = pop_token(rem_tokens);
+		temp = ft_strjoin(filename, token.lexeme);
+		free(filename);
+		free(token.lexeme);
+		if (!temp)
+		{
+			return (NULL);
+		}
+		filename = temp;
+	}
+	return (filename);
+}
+
+static t_tag	get_redir_type(t_list **rem_tokens)
+{
+	t_token	token;
+
+	token = pop_token(rem_tokens);
+	free(token.lexeme);
+	return (token.tag);
+}
+
+static t_error	parse_redir(t_redir **redir, t_list **rem_tokens)
+{
 	t_redir	new_redir;
 
-	// get tag
-	token = pop_token(rem_tokens);
-	new_redir.type = token.tag;
-	free(token.lexeme);
+	new_redir.type = get_redir_type(rem_tokens);
+
 	// check next token
 	if (!*rem_tokens)
 	{
 		ft_printf_fd(STDERR_FILENO, "syntax error: missing redirection target\n");
 		return (ERR);
 	}
-	else if (get_token_tag(*rem_tokens)== WHITESPACE)
+
+	if (get_token_tag(*rem_tokens)== WHITESPACE)
+	{
 		consume_token(rem_tokens);
-	// get filename
+	}
+
+	// check next token
 	if (!*rem_tokens || !is_literal(get_token_tag(*rem_tokens)))
 	{
 		ft_printf_fd(STDERR_FILENO, "syntax error: missing redirection target\n");
 		return (ERR);
 	}
-	token = pop_token(rem_tokens);
-	new_redir.filename = token.lexeme;
+
+	new_redir.filename = get_redir_filename(rem_tokens);
+	if (!new_redir.filename)
+	{
+		return (DEADLY_ERR);
+	}
 
 	*redir = ft_calloc(1, sizeof(t_redir));
 	if (!*redir)
